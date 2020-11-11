cfc649b47887db9324c2b263727d0640f16ea72e
GabrielBrotas <gabrielbrotas22@gmail.com>
2020-11-10 22:00:57 -0300

    db changed

--- Server/src/routes.ts
+++ Server/src/routes.ts
@@ -6,12 +6,22 @@ const routes = express.Router();
 routes.post('/classes', async (req, res) => {
     const {name, avatar, whatsapp, bio, subject, cost, schedule} = req.body
     
-    // inserir usuarios na table uosers
-    await db('users').insert({
+    // inserir usuario na table users, vai retornar o id gerado dos usuarios inseridos na tabela, como só é possivel inserir um usuario por vez vai retornar o id dele em um array
+    // caso quisesse inserir mais de um usuario ao mesmo tempo era só usar um array no insert([{...},{...},...])
+    const insertedUsersId = await db('users').insert({
         name, 
         avatar,
         whatsapp,
         bio
+    })
+
+    // pegar o id do usuario que está no array
+    const user_id = insertedUsersId[0]
+    
+    await db('classes').insert({
+        subject,
+        cost,
+        user_id
     })
 
     return res.send()
