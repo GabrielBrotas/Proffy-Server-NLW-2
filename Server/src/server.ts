66873138340cd949d09dbe541e623a328e7c3e61
GabrielBrotas <gabrielbrotas22@gmail.com>
2020-11-11 21:00:30 -0300

    cors

--- Server/src/server.ts
+++ Server/src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors'
 
 const app = express();
 
@@ -16,9 +17,9 @@ const app = express();
     Query Params: Paginação, filtros, ordenação
 */
 
+app.use(cors())
 app.use(express.json());
 app.use(routes);
-
 
 app.listen(8080, () => {
     console.log('server running in port 8080')
